2e9cf526bee273693318989f0efa23f586e1d8b3
martystucchi95-sudo <marty.stucchi95@gmail.com>
2026-07-17 14:04:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,36 @@
 const CACHE_NAME = 'lunathina-v2';
+const BASE_URL = 'https://martystucchi95-sudo.github.io/LunaThina/';
+
+const ASSETS = [
+  BASE_URL,
+  BASE_URL + 'index.html',
+  BASE_URL + 'manifest.json',
+  BASE_URL + 'icon-192.png',
+  BASE_URL + 'icon-512.png',
+];
 
 self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    ).then(() => clients.claim())
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+    })
   );
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', function(e) {
-  if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).then(function(res) {
-      if (res && res.status === 200) {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-      }
-      return res;
-    }).catch(function() {
-      return caches.match(e.request).then(cached => cached || new Response('offline'));
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
