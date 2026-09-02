190a4a9ef022e12b6e1c70f196fc344f552d1d11
martystucchi95-sudo <marty.stucchi95@gmail.com>
2026-09-02 16:05:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-const CACHE_NAME='lunathina-v3';
+const CACHE_NAME='lunathina-v4';
 const ASSETS=['./', './index.html','./manifest.json','./icon-192.png','./icon-512.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Pacifico&family=Caveat:wght@600;700&family=Dancing+Script:wght@700&display=swap'];
+  'https://unpkg.com/@babel/standalone/babel.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!=CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const cl=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,cl));return r;})));});
