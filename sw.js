740fb08250476ef71554786ac33d9ec0be696ecc
martystucchi95-sudo <marty.stucchi95@gmail.com>
2026-09-15 15:14:18 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME='lunathina-v4';
-const ASSETS=['./', './index.html','./manifest.json','./icon-192.png','./icon-512.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!=CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const cl=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,cl));return r;})));});
